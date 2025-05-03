f0acc80ef74488a77122fa203db139b6820809d1
Littlenine小九 <Aaron_root@outlook.com>
2025-05-03 15:43:18 +0800

    Add Files

---
+++ sync.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+BUILD_ROOT="$PWD"
+QSSI_ROOT="${BUILD_ROOT}/qssi"
+VENDOR_ROOT="${BUILD_ROOT}/vendor"
+LE_ROOT="${BUILD_ROOT}/le"
+
+function sync_repo {
+    mkdir -p "$1" && cd "$1"
+    echo "[+] Changed directory to $1."
+
+    if repo init --depth=1 -q -u https://github.com/LittlenineEnnea/SM8650_BSP_Sync -b main -m "$2"; then
+        echo "[+] Repo initialized successfully."
+    else
+        echo "[-] Error: Failed to initialize repo."
+        exit 1
+    fi
+
+    echo "[+] Starting repo sync..."
+    if schedtool -B -e ionice -n 0 repo sync -q -c --force-sync --optimized-fetch --no-tags --retry-fetches=5 -j"$(nproc --all)"; then
+        echo "[+] Repo synced successfully."
+    else
+        echo "[-] Error: Failed to sync repo."
+        exit 1
+    fi
+}
+
+sync_repo "$QSSI_ROOT" "qssi.xml"
+sync_repo "$VENDOR_ROOT" "target.xml"
+sync_repo "$LE_ROOT" "le.xml"
+
+cd "$BUILD_ROOT"
+echo "[+] Successfully returned to the build root."
